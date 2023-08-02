d88e3d7cb4a8df903bc9b9d4f859fe007d72e8e5
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-02 12:23:28 +0530

    [jogan] Binary search tree created as shown in image

--- HashTableAndBinaryTree/Program.cs
+++ HashTableAndBinaryTree/Program.cs
@@ -9,10 +9,17 @@ public class Program
         BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
         binarySearch.Insert(30);
         binarySearch.Insert(70);
+        binarySearch.Insert(22);
+        binarySearch.Insert(40);
+        binarySearch.Insert(60);
+        binarySearch.Insert(95);
+        binarySearch.Insert(11);
+        binarySearch.Insert(65);
+        binarySearch.Insert(3);
+        binarySearch.Insert(16);
+        binarySearch.Insert(63);
+        binarySearch.Insert(67);
         binarySearch.Display();
-        Console.WriteLine("\n");
-
-
 
         //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
         //hash.Add("0", "To");
