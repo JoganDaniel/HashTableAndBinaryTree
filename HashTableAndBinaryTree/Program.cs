7c3f021ea9b1096b7f43e619d72c66c49086d503
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-02 12:21:04 +0530

    [jogan] Binary search tree created

--- HashTableAndBinaryTree/Program.cs
+++ HashTableAndBinaryTree/Program.cs
@@ -1,21 +1,31 @@
-﻿using HashTableAndBinaryTree.HashTable;
+﻿using HashTableAndBinaryTree.BinaryTree;
+using HashTableAndBinaryTree.HashTable;
 using System;
 public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("HashTable and binary tree");
-        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-        hash.Add("0", "To");
-        hash.Add("1", "be");
-        hash.Add("2", "or");
-        hash.Add("3", "not");
-        hash.Add("4", "to");
-        hash.Add("5", "be");
-        string hash5 = hash.Get("5");
-        Console.WriteLine("5th index value : " + hash5);
-        hash.Remove("2");
-        string hash2 = hash.Get("2");
-        Console.WriteLine("2th index value : " + hash2);
+        BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+        binarySearch.Insert(30);
+        binarySearch.Insert(70);
+        binarySearch.Display();
+        Console.WriteLine("\n");
+
+
+
+        //MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        //hash.Add("0", "To");
+        //hash.Add("1", "be");
+        //hash.Add("2", "or");
+        //hash.Add("3", "not");
+        //hash.Add("4", "to");
+        //hash.Add("5", "be");
+        //string hash5 = hash.Get("5");
+        //Console.WriteLine("5th index value : " + hash5);
+        //hash.Remove("2");
+        //string hash2 = hash.Get("2");
+        //Console.WriteLine("2th index value : " + hash2);
+
     }
 }
